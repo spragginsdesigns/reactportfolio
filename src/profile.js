89c21dde3625fe858d3c18d959a394e7782d5c31
Austin Spraggins <43624199+spragginsdesigns@users.noreply.github.com>
2022-04-16 21:06:19 -0700

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -19,7 +19,7 @@ const background = {
 const section2title = "About Who I Am";
 const about = {
   paragraph:
-    "I have been learning web programming and cyber security for three years now, and I fell in love with it. I build websites, web and mobile applications, and pentest them so your website will not be vulnerable to hackers. With Spraggins Designs, you will hire a person who thrives in programming for the web, a graphics designer, OSCP certified pentester and marketer for your brand. I am a business owner, graphics designer, YouTuber, and primarily self-taught, independent programmer with a wide range of skills showcased below and a professional YouTube Content Creator.",
+    "I am a professional web designer and web developer as well as Penetration Tester through Kali Linux. I was a truck driver three years ago, and I quit to do this. Design and develop websites, web apps, mobile apps and secure them as well as the rest of the web as much as I can. I provide services as a business and am seeking employment as well.",
 };
 // My skills, I did not add any skill bars.
 const skillsBar = [
@@ -33,7 +33,7 @@ const skillsBar = [
     name: "CSS3",
     faClass: "fab fa-css3",
   },
-   {
+  {
     name: "SASS",
     faClass: "fab fa-sass",
   },
@@ -41,6 +41,11 @@ const skillsBar = [
     name: "JavaScript",
     faClass: "fab fa-js",
   },
+   {
+    name: "jQuery",
+    faClass: "fab fa-jquery",
+  },
+
   // Or use fab fa-npm for npm icon //
   {
     name: "Node",
@@ -82,9 +87,60 @@ const skillsBar = [
 
     faClass: "fab fa-linux",
   },
-];
+  {
+
+    name: "Web Development",
+    faClass: "fab faClass: "fab fa-web-dev",
+    },
 // My main projects showcased first
-const section3Title = "Main Projects";
+const section3Title = "Professional Projects";
+const projects = [
+  {
+    //  './styles/images.css' in #project1
+    id: "project1",
+    name: "Effycient Homepage",
+    skills: ["Adobe Creative Cloud, SEO, Branding/Marketing"],
+    url: "https://www.youtube.com/c/shadowgaming99",
+  },
+  {
+    // './styles/images.css' in #project2
+    id: "project2",
+    name: "SASS Portfolio",
+    skills: ["HTML, CSS, SASS JS"],
+    url: "https://www.austinspragginsportfolio.xyz",
+  },
+  {
+    // './styles/images.css' in #project3
+    id: "project3",
+    name: "Spraggins Designs Logo",
+    skills: ["Adobe Creative Cloud Apps"],
+    url: "https://www.spragginsdesigns.com",
+  },
+  {
+    //  './styles/images.css' in #project4
+    id: "project4",
+    name: "My Wife's Website",
+    skills: ["HTML, CSS, JS, Parallax"],
+    url: "https://spragginsdesigns.github.io/jessica-spraggins-portfolio/",
+  },
+  {
+    //  './styles/images.css' in #project5
+    id: "project5",
+    name: "Shadow Gaming | Official Website",
+    skills: ["HTML, CSS, JS"],
+    url: "https://www.shadowgaming.network/",
+  },
+  {
+    //  './styles/images.css' in #project6
+    id: "project6",
+    name: "Rock Paper Scissors Game | Created With My Daughter Lilly",
+    skills: ["Python 3.9"],
+    url: "https://github.com/spragginsdesigns/rock-paper-scissors",
+  },
+];
+
+// My passion projects
+const section3Title = "Passion Projects";
 const projects = [
   {
     //  './styles/images.css' in #project1
@@ -168,7 +224,8 @@ const social = {
   twitter: "https://twitter.com/shadow_gaming99",
   instagram: "https://www.instagram.com/spragginsdesigns",
   linkedin: "https://www.linkedin.com/in/austin-spraggins-a0514612a",
-  resume: "https://drive.google.com/file/d/1TlroSotc1D08paOu9Pq64a4s__UaGXKQ/view?usp=sharing",
+  resume:
+    "https://drive.google.com/file/d/1TlroSotc1D08paOu9Pq64a4s__UaGXKQ/view?usp=sharing",
 };
 
 export {
